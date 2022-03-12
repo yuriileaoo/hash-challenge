eb9d4217d9e0914cf465ecfc1064aa7f63eb947f
Yuri <eu@yurileao.com>
2022-03-12 17:50:15 -0300

    fix: client grpc connection

--- src/infra/http/grpc/client.ts
+++ src/infra/http/grpc/client.ts
@@ -3,6 +3,6 @@ import * as grpc from "@grpc/grpc-js";
 import { DiscountClient } from "../../../../proto/discount_grpc_pb";
 
 export default new DiscountClient(
-  `localhost:${process.env.PORT_GRPC}`,
+  `discount:${process.env.PORT_GRPC}`, // appDiscount
   grpc.credentials.createInsecure()
 );
